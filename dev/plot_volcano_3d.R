505792423118d5c1be6b091c12e52d031505026f
Jordan Soubrier <jordan.soubrier@gmail.com>
2025-09-26 21:05:54 +0200

    3d and profile volcano plots with palette height perception distortion

--- dev/plot_volcano_3d.R
+++ dev/plot_volcano_3d.R
@@ -1,4 +1,5 @@
-#' Plot the volcano dataset in 3D using plotly
+#' Rescale the 'volcano' dataset to reflect the perceptual error induced
+#' by the given palette
 #'
 #' @param color_palette Character vector of hex colors for the surface gradient.
 #'   Defaults to "terra" palette which is good for terrain visualization.
@@ -16,25 +17,32 @@
 #' @importFrom stats approx
 rescale_to_pal <- function(values, palette) {
   # Get palette info with CIEDE2000 differences
-  pal_info <- palette_info(palette)
+  pal_info <- hex2palette_info(palette)
 
   # Calculate cumulative differences
-  cum_diffs <- cumsum(c(0, pal_info$delta_2000))
+  cum_diffs <- pal_info$cum_delta_2000
+  # cum_diffs <- seq_len(length(palette))
 
   # Normalize input values to [0,1]
   vals_norm <- (values - min(values)) / diff(range(values))
 
   # Create interpolation function
-  height_factor <- approx(
+  height_factors <- approx(
     x = seq(0, 1, length.out = length(palette)),
     y = cum_diffs / max(cum_diffs),
     xout = vals_norm
   )$y
 
+  hf_matrix <- matrix(
+    height_factors,
+    nrow = nrow(values)
+  )
+
+  # return(hf_matrix)
   # Apply the height factors to original scale
   values_range <- diff(range(values))
   values_min <- min(values)
-  values_min + height_factor * values_range
+  values_min + hf_matrix * values_range
 }
 
 #' Plot the volcano dataset in 3D using plotly
@@ -44,17 +52,22 @@ rescale_to_pal <- function(values, palette) {
 #' @param show_contours Logical, whether to show contour lines (default = TRUE)
 #' @param pal_rescale Logical, whether to rescale heights based on perceptual
 #'   differences in the palette (default = FALSE)
+#' @param z_multiplier Numerical, multiplier to apply to the height to magnify
+#'   or decrease the perceived height.
+#' @param bw Logical, indicates if the palette should be ignored and plot in grayscale.
 #' @return A plotly 3D surface plot object
 #' @import plotly
 #' @importFrom datasets volcano
 #' @importFrom grDevices hcl.colors
 #' @export
-plot_volcano_3d <- function(color_palette = hcl.colors(n = 16, palette = "terrain"),
+plot_volcano_3d <- function(color_palette = hcl.colors(n = 256, palette = "terrain"),
                             show_contours = TRUE,
-                            pal_rescale = FALSE) {
+                            pal_rescale = FALSE,
+                            z_multiplier = 1L,
+                            bw = FALSE) {
   # Create sequence for x and y coordinates in meters (10m grid)
-  x_seq <- (seq_len(nrow(volcano)) - 1) * 10
-  y_seq <- (seq_len(ncol(volcano)) - 1) * 10
+  x_seq <- (seq_len(ncol(volcano)) - 0.5) * 10
+  y_seq <- (seq_len(nrow(volcano)) - 0.5) * 10
 
   # Prepare Z values
   z_values <- if (pal_rescale) {
@@ -63,13 +76,17 @@ plot_volcano_3d <- function(color_palette = hcl.colors(n = 16, palette = "terrai
     volcano
   }
 
+  if (bw) {
+    color_palette <-
+      scico::scico(n = length(color_palette), palette = "grayC")
+  }
   # Create colorscale in plotly format
-  colorscale <- lapply(seq_along(color_palette), function(i) {
-    list(
-      (i - 1) / (length(color_palette) - 1),
-      color_palette[i]
-    )
-  })
+  # colorscale <- lapply(seq_along(color_palette), function(i) {
+  #   list(
+  #     (i - 1) / (length(color_palette) - 1),
+  #     color_palette[i]
+  #   )
+  # })
 
   # Create the plot with contours included
   p <- plotly::plot_ly(
@@ -77,7 +94,8 @@ plot_volcano_3d <- function(color_palette = hcl.colors(n = 16, palette = "terrai
     x = ~x_seq,
     y = ~y_seq,
     z = ~z_values,
-    colorscale = colorscale,
+    # colorsale = colorscale,
+    colors = color_palette,
     showscale = TRUE,
     contours = if (show_contours) {
       list(
@@ -85,7 +103,7 @@ plot_volcano_3d <- function(color_palette = hcl.colors(n = 16, palette = "terrai
           show = TRUE,
           usecolormap = TRUE,
           highlightcolor = "#ffffff",
-          width = 2,
+          width = 8,
           project = list(z = TRUE)
         )
       )
@@ -98,40 +116,51 @@ plot_volcano_3d <- function(color_palette = hcl.colors(n = 16, palette = "terrai
   y_range <- diff(range(y_seq))
   z_range <- diff(range(volcano))
 
+  print(glue::glue("Rangos:\nx:{x_range}\ny:{y_range}\nz:{z_range}"))
+
   # # Normalize so largest horizontal dimension is 1
   max_xy <- max(x_range, y_range)
   x_scale <- x_range / max_xy
   y_scale <- y_range / max_xy
   # # Make vertical scale
-  z_scale <- z_range / max_xy
+  z_scale <- z_range * z_multiplier / max_xy
 
+  print(glue::glue("Escalas:\nx:{x_scale}\ny:{y_scale}\nz:{z_scale}"))
   # Configure layout
   p |> plotly::layout(
     scene = list(
+      width = 960,
+      height = 540,
+      # autosize = TRUE,
+      autosize = FALSE,
       camera = list(
-        eye = list(x = 1.87, y = 0.88, z = 0.64),
+        eye = list(x = 0.85, y = 0.55, z = 0.35),
+        # eye = list(x = 1.5, y = 0, z = 0),
+        # up = list(x = 0, y = 0, z = 1),
+        # eye = list(x = 0.87 * x_scale, y = 0.87 * y_scale, z = 0.87),
         center = list(x = 0, y = 0, z = -0.2)
       ),
       aspectmode = "manual",
       aspectratio = list(
-        x = 1,
-        y = 1,
-        z = 1 # Use 1:1:1 ratio since all dimensions are in meters
+        x = x_scale,
+        y = y_scale,
+        z = z_scale
       ),
       xaxis = list(
-        title = "X (meters)",
-        range = c(min(x_seq), max(x_seq))
+        title = "X (meters)" # ,
+        # range = c(min(x_seq), max(x_seq))
       ),
       yaxis = list(
-        title = "Y (meters)",
-        range = c(min(y_seq), max(y_seq))
+        title = "Y (meters)" # ,
+        # range = c(min(y_seq), max(y_seq))
       ),
       zaxis = list(
-        title = "Elevation (meters)",
-        range = c(min(z_values), max(z_values))
+        title = "Elevation (meters)" # ,
+        # range = c(min(z_values), max(z_values))
       )
     ),
-    margin = list(t = 40),
+    margin = list(l = 10, r = 5, b = 0, t = 30),
+    # margin = list(t = 40),
     title = "Volcano Dataset 3D Visualization"
   )
 }
---
+++ dev/profile_plots.R
@@ -0,0 +1,42 @@
+pal <- scico::scico(n = 256, palette = "batlow")
+pal <- rainbow(n = 256)
+pal <- colorRampPalette(igepal_hex2)(256)
+pal <- colorRampPalette(igepal_hex2, space = "Lab")(256)
+pal <- unipals::extend_palette_equal_ciede2000(igepal_hex2, 256)$hex
+pals::pal.bands(pal)
+
+plot_volcano_3d(color_palette = pal, bw = TRUE)
+plot_volcano_3d(color_palette = pal, pal_rescale = TRUE, bw = TRUE)
+
+rescale_to_pal(volcano, pal) |> head()
+volcano |> head()
+87 * 61
+
+rescale_to_pal(volcano, pal) |>
+  as.matrix(nrow = nrow(volcano)) |>
+  str()
+c(1, 2, 4, 5) |> as.matrix(ncol = 2, dimnames = c("x", "y"))
+dim(c(1, 2, 3, 4)) <- c(2, 2)
+# Volcano profile passing through the crater bottom
+min_pos <- which(volcano == 148, arr.ind = TRUE) |>
+  as.data.frame() |>
+  dplyr::filter(row %in% 20:40, col %in% 20:40)
+
+original <- volcano[, 34]
+
+induced <- rescale_to_pal(as.matrix(volcano[, 34]), pal) |> as.vector()
+
+x_profile <- (seq_len(nrow(volcano)) - 0.5) * 10
+
+profile_data <- tibble::tibble(
+  x = x_profile,
+  original,
+  induced
+) |>
+  tidyr::pivot_longer(c(original, induced), names_to = "profile", values_to = "height")
+
+profile_data |>
+  ggplot2::ggplot(
+    ggplot2::aes(x = x, y = height, color = profile)
+  ) +
+  ggplot2::geom_line()
