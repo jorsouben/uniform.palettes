1b997b818ace5a8d0a91081bb783f9cd01d38655
Jordán Soubrier <jordan.soubrier@gmail.com>
2025-10-02 11:28:09 +0200

    mono gradient now takes B and W as extremes

--- R/mono_gradient.R
+++ R/mono_gradient.R
@@ -2,16 +2,23 @@
 #' the function extend_palette_equal_ciede2000 for fixed extremes)
 #'
 #' @param n, Integer, number of output colors
-#' @param from Character, hex color code of the initial color.
-#' @param to Character, hex color code of the end color. Defaults to 'white'.
+#' @param ref Character, hex color code of the base color. Defaults to 'PANTONE 7461 C'.
+#' @param from Character, hex color code of the darkest color. Defaults to 'black'.
+#' @param to Character, hex color code of the brightest color. Defaults to 'white'.
+#' #param pre_rgb_interpolation Logical, whether to interpolate in RGB space prior to equalization.
 #'
-#' @returns An equalized color gradient palette in hex format
+#' @returns An equalized color gradient monocolor palette in hex format
 #' @export
-mono_gradient <- function(n, from = igepal_hex[2], to = "FFFFFF") {
-  extend_palette_equal_ciede2000(
-    c(from, to),
+mono_gradient <- function(
     n,
-    fixed_hex = from,
-    mode = "global" # "anchor_exact"
+    from = "#000000",
+    ref = igepal_hex[2],
+    to = "#FFFFFF",
+    pre_rgb_interpolation = TRUE) {
+  extend_palette_equal_ciede2000(
+    c(from, ref, to),
+    n,
+    fixed_hex = ref,
+    mode = "anchor_exact"
   )
 }
