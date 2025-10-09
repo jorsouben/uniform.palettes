f160900fa54daf1a3a5645399afab9ae532d1906
Jordán Soubrier <jordan.soubrier@gmail.com>
2025-10-09 11:40:27 +0200

    esbozo añadidos

--- dev/perception error diags.R
+++ dev/perception error diags.R
@@ -1,3 +1,20 @@
+# TODO:
+# 1. Funciones de diagnóstico:
+#   1.1 local_visual_error (en % o no): adrel_global
+#   1.2 error rms (lista, documentar)
+# 2. funcion perceptual_error_rms() coge ColorMaps o hex o dfs.
+#    Es interesante que funcione fuera del objeto para tener felexibiliad
+#    al aplicarlo. Pero al final siempre convierte primero a ColorMap
+#    Lo ideal sería (con resto de funciones tambien) hacerlo método
+#    y hacer un wrapper con as_colormap -> método para el uso flexible
+#    y cómodo de la funcion
+# 3. Plots de canales
+# 4. plots de deltas/cum_deltas
+#  5. Varianzas deltas, cv, rango
+#  6. Revisar funcion equalizacion, que sea correcta. Comparar si es posible con
+#  el método antiguo.
+
+
 percl <- function(pal, medida = "cie2000") {
   pal <- as_colormap(pal)
   deltas <- pal$deltas(medida)[-1]
@@ -71,5 +88,53 @@ diag |>
 
 which(diag$valor == max(diag$valor))
 max(diag$valor)
-[1] 3.301948
-[1] 0.2022716
+# [1] 3.301948
+# [1] 0.2022716
+
+testcolors <-
+  data.frame(
+    r = c(0, 0, 0, 0, 100, 205),
+    g = c(0, 0, 100, 205, 0, 0),
+    b = c(100, 205, 0, 0, 0, 0)
+  )
+# devtools::load_all()
+testpal <- testcolors |> as_colormap()
+testpal$plot_swatch()
+testpal$deltas()
+
+# Vése máis diferencia no verde. O azul ten franxa máis estreita e mais cara ao celeste
+# O vermello máis cara ao amarelo.
+# ;
+# https://support.hunterlab.com/hc/en-us/categories/201319586-Color-Theory
+#
+# batpal$deltas() |> plot()
+# > batpal$deltas()[-1] |> plot()
+# > batpal$deltas()[-1] |> min()
+# [1] 0.5267951
+# > batpal$deltas()[-1] |> max()
+# [1] 0.5820789
+# > ecuige$deltas()[-1] |> max()
+# Error: objeto 'ecuige' no encontrado
+# > eqige$deltas()[-1] |> min()
+# [1] 0.4500861
+# > eqige$deltas()[-1] |> max()
+# [1] 0.5677183
+# > equalize(eqige, n = 256) -> eqige2
+# Defaulting to LAB.
+# Check your data if that's not correct
+# > eqige2$deltas() |> plot()
+# > eqige2$deltas()[-1] |> plot()
+# > eqige2$deltas()[-1] |> var()
+# [1] 0.0002692024
+# > eqige2$deltas()[-1] |> var()
+# [1] 0.0002692024
+# > eqige$deltas()[-1] |> var()
+# [1] 0.0004631088
+# > eqige2$deltas()[-1] |> var()/mean(eqige2$deltas[-1])
+# Error en eqige2$deltas[-1]: objeto de tipo 'closure' no es subconjunto
+# > eqige2$deltas()[-1] |> var()/mean(eqige2$deltas()[-1])
+# [1] 0.0005261545
+# > eqige$deltas()[-1] |> var()/mean(eqige$deltas()[-1])
+# [1] 0.0009051433
+# > batpal$deltas()[-1] |> var()/mean(batpal$deltas()[-1])
+# [1] 0.0001459367
